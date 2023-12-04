d84886239d01dacc269c989cb87f7158053faac1
Ulysses Wu <wdwxy12345@gmail.com>
2023-12-05 02:51:37 +0800

    upgrade to `net8`

--- FreeMote.FastLz/AssemblyInfo.cpp
+++ FreeMote.FastLz/AssemblyInfo.cpp
@@ -9,7 +9,7 @@ using namespace System::Security::Permissions;
 [assembly:AssemblyConfigurationAttribute(L"")];
 [assembly:AssemblyCompanyAttribute(L"")];
 [assembly:AssemblyProductAttribute(L"FreeMote.FastLz")];
-[assembly:AssemblyCopyrightAttribute(L"Copyright (c) Ulysses & Ariya Hidaya 2022")];
+[assembly:AssemblyCopyrightAttribute(L"Copyright (c) Ulysses & Ariya Hidaya 2022-2024")];
 [assembly:AssemblyTrademarkAttribute(L"wdwxy12345@gmail.com")];
 [assembly:AssemblyCultureAttribute(L"")];
 
